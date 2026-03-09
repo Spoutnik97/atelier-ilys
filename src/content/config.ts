d86f1331703cc48ad925b92fa7d2546c41b234e7
Guillaume Piedigrossi <30442116+Spoutnik97@users.noreply.github.com>
2026-03-09 21:59:23 +0100

    fix calendar config

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,22 +1,22 @@
-import { defineCollection, z } from 'astro:content'
+import { defineCollection, z } from "astro:content";
 
 const calendarCollection = defineCollection({
-  type: 'data',
+  type: "data",
   schema: z.object({
     events: z.array(
       z.object({
         startDate: z.string(),
         endDate: z.string().optional(),
         title: z.string(),
-        type: z.enum(['indigo', 'teinture', 'ecoprint', 'pigments', 'famille', 'beaux-arts']),
+        type: z.string(),
         description: z.string().optional(),
         price: z.string().optional(),
         spots: z.number().optional(),
-      })
+      }),
     ),
   }),
-})
+});
 
 export const collections = {
   calendar: calendarCollection,
-}
+};
